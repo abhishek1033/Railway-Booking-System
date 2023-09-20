cdb3992fa68a221fbcda0352ed9fc5f3d975c4ae
Abhishek Tiwari <abhishek.tiwari.15@alight.com>
2023-09-20 18:04:38 +0530

    add discovery server project

---
+++ Back-End/Discovery-Server/src/test/java/com/railway/discovery/server/Discovery/Server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railway.discovery.server.Discovery.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
